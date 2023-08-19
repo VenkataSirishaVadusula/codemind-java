95c4b0629b9dd1a1b7b0920770a7266c726bb53a
VenkataSirishaVadusula <119840085+VenkataSirishaVadusula@users.noreply.github.com>
2023-08-19 11:57:18 +0530

    import java.util.*; public class Me {     public static void main(String[] args) {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         //Array declaration         int[] intArray = new int[n];         for(int i=0;i<n;i++) {             intArray[i] = sc.nextInt();         }         int sum=0;         for(int eachElement:intArray) {             sum +=eachElement;         }         System.out.println(sum);     } }

---
+++ Sum_of_Array.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Me {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        //Array declaration
+        int[] intArray = new int[n];
+        for(int i=0;i<n;i++) {
+            intArray[i] = sc.nextInt();
+        }
+        int sum=0;
+        for(int eachElement:intArray) {
+            sum +=eachElement;
+        }
+        System.out.println(sum);
+    }
+}
